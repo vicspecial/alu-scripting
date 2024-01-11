73a5c076abd32045dcc79c0eaa8599eb1862debb
Victoria <v.godstime@alustudent.com>
2024-01-11 05:38:31 -0800

    simply

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,5 +1,12 @@
 ruby
 #!/usr/bin/env ruby
-regex = /School/
-puts ARGV[0].scan(regex).join
 
+input = ARGV[0] # Get the input argument
+
+def match_school(input)
+  regex = /School/ # Regular expression to match "School"
+  match = input.scan(regex) # Use scan method to find all occurrences of the regular expression in the input
+  puts match.join # Output the matched string
+end
+
+match_school(input) # Call the method with the input argument
